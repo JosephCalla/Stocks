ec335bf9ae2edcf898ac58fbd785a624a6f213c4
Joseph Calla <jcalla.consultant@newscorp.com>
2022-10-04 14:40:51 -0500

    NOTICKET: News header view

---
+++ Stocks/Controllers/NewsViewController.swift
@@ -0,0 +1,109 @@
+//
+//  NewsViewController.swift
+//  Stocks
+//
+//  Created by Joseph Estanislao Calla Moreyra on 4/10/22.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    
+    var tableView: UITableView = {
+        let table = UITableView()
+        // Register cell, header
+        table.register(NewsHeaderView.self, forHeaderFooterViewReuseIdentifier: NewsHeaderView.identifier)
+        table.backgroundColor = .clear
+        return table
+    }()
+    
+    private let type: Type
+    
+    enum `Type` {
+        case topStories
+        case company(symbol: String)
+        
+        var title: String {
+            switch self {
+            case .topStories:
+                return "Top Stories"
+            case .company(symbol: let symbol):
+                return symbol.uppercased()
+            }
+        }
+    }
+    
+    // MARK: - Initializer
+    init(type: Type) {
+        self.type = type
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTable()
+        fetchNews()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    // MARK: - Private functions
+    private func setupTable() {
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func fetchNews() {
+        
+    }
+    
+    private func open(url: URL) {
+        
+    }
+
+}
+
+extension NewsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let header = tableView.dequeueReusableHeaderFooterView(
+            withIdentifier: NewsHeaderView.identifier
+        ) as? NewsHeaderView else {
+            return nil
+        }
+        
+        header.configure(with: .init(
+            title: self.type.title,
+            shouldShowAddButton: false
+        ))
+        
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return NewsHeaderView.preferredHeight
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
